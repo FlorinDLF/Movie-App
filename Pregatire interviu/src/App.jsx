a9a2f54bcac19ea8deeccbe5beeff790d73816bd
David Florin <leonard.david@stud.acs.upb.ro>
2025-09-13 19:31:58 +0300

    Implement database using Appwrite for the metrics of searches

--- Pregatire interviu/src/App.jsx
+++ Pregatire interviu/src/App.jsx
@@ -1,35 +1,104 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Search from './components/Search.jsx'
+import Spinner from './components/Spinner.jsx';
+import { useState, useEffect } from 'react'
+import MovieCard from './components/MovieCard.jsx';
+import { useDebounce } from 'react-use';
+import { updateSeacrhCounts } from './appwrite.js';
 
-function App() {
-  const [count, setCount] = useState(0);
+const API_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+};
+
+const App = () => {
+  const[searchTerm, setSearchTerm] = useState('');
+
+  const[errorMessage, setErrorMessage] = useState('');
+
+  const[moviesList, setMoviesList] = useState([]);
+
+  const[loading, setLoading] = useState(false);
+
+  const[debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+  useDebounce( () => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
+
+  const fetchMovies = async (query = '') => {
+    setLoading(true);
+    setErrorMessage('');
+
+    try {
+      const endpoint = query ? `${API_URL}/search/movie?query=${encodeURIComponent(query)}` : `${API_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+
+      if (data.response === 'False') {
+        setErrorMessage(data.error || 'Failed to fetch movies. Please try again later.');
+        setMoviesList([]);
+        return;
+      }
+      setMoviesList(data.results || []);
+
+      if(query && data.results.length > 0) {
+        await updateSeacrhCounts(query, data.results[0]);
+    }
+    } catch(err) {
+      console.error('Error fetching : ${err}');
+      setErrorMessage('Failed to fetch movies. Please try again later.');
+    }
+    finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="pattern"/>
+
+
+      <div className = "wrapper">
+        <header>
+          <img src="./hero-img.png" alt = "Hero Banner"/>
+
+          <h1>Find <span className='text-gradient'>movies</span> you enjoy</h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        </header>
+
+        <section className="all-movies">
+          <h2 className='mt-[40px]'>All movies</h2>
+
+          {loading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className='text-white'>{errorMessage}</p>
+          ) : (
+            <ul>
+              {moviesList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie}/>
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count - 17)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    </main>
+  )
 }
 
-export default App;
+export default App
